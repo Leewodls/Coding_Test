348ac7e1521c044cc1a19e4edd5bf1944e03ea26
이재인 <wodls2436@gmail.com>
2025-02-17 22:52:17 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 55 ms, Memory: 48,256 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,9 +1,10 @@
 T = 10
-
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     tc = int(input())
     n, m = map(int, input().split())
-    total=1
+    result = 1
     for i in range(m):
-        total = total*n
-    print(f"#{tc} {total}")
+        result = result*n
+        
+    print(f"#{tc} {result}")
