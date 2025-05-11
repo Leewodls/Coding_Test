9ebde1081f6d2caaf099394a24c22b0a74371a97
이재인 <wodls2436@gmail.com>
2025-05-11 21:37:19 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,30 @@
+
+# def solution(storey):
+#     result = 0
+#     while storey > 0:
+#         num = storey % 10
+#         if num >= 5:
+#             num = 10 - num    
+#             result+=num
+#             storey = (storey // 10) + 1  
+#         else:  
+#             result += num
+#             storey //= 10
+#     return result
+
+def solution(storey):
+    result = 0
+    while storey > 0:
+        num = storey % 10  
+        next_num = (storey // 10) % 10  
+        
+        
+        if num > 5 or (num == 5 and next_num >= 5):
+            result += 10 - num
+            storey = (storey // 10) + 1  
+        else:
+            result += num
+            storey //= 10  
+    return result
+
+
